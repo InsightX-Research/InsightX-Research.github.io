f6226f2088adbf425aa39058079863069b6fb279
Chhaya-Distributors <dishantsingh.mit@gmail.com>
2025-04-07 22:46:17 +0530

    Create BookViewer.tsx

---
+++ BookViewer.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import HTMLFlipBook from "react-pageflip";
+import { pages } from "./pages";
+import "./BookViewer.css";
+
+const BookViewer = () => {
+  return (
+    <div className="book-container">
+      <h1 className="book-title">📖 My Book</h1>
+      <HTMLFlipBook
+        width={400}
+        height={550}
+        showCover={true}
+        className="flipbook"
+      >
+        {pages.map((page, index) => (
+          <div className="page" key={index}>
+            <h2>{page.title}</h2>
+            <p>{page.content}</p>
+            <footer>Page {index + 1}</footer>
+          </div>
+        ))}
+      </HTMLFlipBook>
+    </div>
+  );
+};
+
+export default BookViewer;
